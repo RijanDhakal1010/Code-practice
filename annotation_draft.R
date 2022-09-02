42d5e2145e860d3f82396ce23a58890e96b611ce
Workstation <workstation@pop-os.localdomain>
2022-09-02 16:55:58 -0500

    getting closer to pasting numbers on next to nodes

--- annotation_draft.R
+++ annotation_draft.R
@@ -4,12 +4,12 @@ p <- read.tree("tree_maybe.tre")
 
 data_file <- read.table(file = 'Base_clade_results.txt', sep = '\t', header = TRUE)
 
-p = ggtree(p) %<+%  data_file
+p = ggtree(p, branch.length = 'none') %<+%  data_file
 
-#ggtree(p,branch.length = 'none') + theme_tree2()+geom_tiplab(align=TRUE, linesize=.5)+ xlim(0,25)
+p + theme_tree2()+geom_tiplab(align=TRUE, linesize=.5)+ xlim(0,25)
 
-#p + geom_text(aes(label=Increase), hjust=1, vjust=1.4, size=3)
+p +  geom_text(aes(label=Increase), hjust=1, vjust=1.4, size=3)
 
-p + geom_text(aes(label=Taxa), hjust=1, vjust=-0.4, size=3) + geom_text(aes(label=Increase), hjust=1, vjust=1.4, size=3)
+p + theme_tree2()+geom_tiplab(align=TRUE, linesize=.5)+ xlim(0,25) +  geom_text(aes(label=Increase), hjust=1, vjust=1.4, size=3)
 
-print(p)
+ggsave("numbers_hopefully.pdf" , width= 60, height = 80, units = "cm", limitsize = FALSE)
